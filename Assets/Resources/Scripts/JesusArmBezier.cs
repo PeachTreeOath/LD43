f4ea2465476b08cc34d4085b4a4494425cdea51e
Thomas <yuyanglai88@gmail.com>
2018-12-01 01:21:16 -0800

    Jesus Arm Math

---
+++ Assets/Resources/Scripts/JesusArmBezier.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class JesusArmBezier : MonoBehaviour {
+
+    public bool isRightArm;
+    public Vector3 armStartPosition;
+    public Transform armEndPosition;
+    public float currentArmEndTangent;
+    public LineRenderer lineRenderer;
+
+    private static int NUM_ARM_POINTS = 30;
+    // private static Vector3 RIGHT_ARM_START_POSITION = new Vector3(0.5f, -4.5f, 0);
+    private static float NEUTRAL_ARM_SLOPE = 1;
+    private static float LOW_ARM_SLOPE = 0.5f;
+    private static float HIGH_ARM_SLOPE = 2;
+    private static float SLOPE_ITERATION_STEP = 0.1f;
+    private Vector3[] mArmPositions = new Vector3[NUM_ARM_POINTS];
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer.positionCount = NUM_ARM_POINTS;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateEndTangent();
+        DrawCurve(armStartPosition, armEndPosition.position, currentArmEndTangent);
+	}
+
+    // End tangent is different based on the driving direction
+    private void UpdateEndTangent() {
+        float endTangent;
+        if (Input.GetKey("d")) { // turning right
+            if (isRightArm) { // high negative
+                endTangent = -1f * HIGH_ARM_SLOPE;
+            } else { // low positive
+                endTangent = LOW_ARM_SLOPE;
+            }
+        } else if (Input.GetKey("a")) { // turning left
+            if (isRightArm) { // low negative
+                endTangent = -1f * LOW_ARM_SLOPE;
+            } else {
+                endTangent = HIGH_ARM_SLOPE;
+            }
+        } else {
+            endTangent = isRightArm ? NEUTRAL_ARM_SLOPE * -1.0f : NEUTRAL_ARM_SLOPE;
+        }
+
+        if (endTangent > currentArmEndTangent && endTangent - currentArmEndTangent > SLOPE_ITERATION_STEP) {
+            currentArmEndTangent = currentArmEndTangent + SLOPE_ITERATION_STEP;
+        } else if (endTangent < currentArmEndTangent && currentArmEndTangent - endTangent > SLOPE_ITERATION_STEP) {
+            currentArmEndTangent = currentArmEndTangent - SLOPE_ITERATION_STEP;
+        }
+    }
+
+    private void DrawCurve(Vector3 startPoint, Vector3 endPoint, float endTangent) {
+        // Calculate slope of forearm
+        float startTangent = 0f;
+
+        // Calculate intersect
+        float cEndPoint = endPoint.y - endTangent * endPoint.x;
+        float intersectX = (startPoint.y - cEndPoint) / endTangent;
+        float intersectY = startPoint.y;
+        Vector3 intersectPoint = new Vector3(intersectX, intersectY, 0);
+
+        // Plot Bezier Curve
+        for (int i = 0; i < NUM_ARM_POINTS; i++) {
+            float t = (float)i / (float)NUM_ARM_POINTS;
+            mArmPositions[i] = CalculateArmPoint(t, startPoint, intersectPoint, endPoint);
+        }
+        lineRenderer.SetPositions(mArmPositions);
+    }
+
+    // Calculate arm point using Quadratic Bezier Function
+    // https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+    private Vector3 CalculateArmPoint(float t, Vector3 startPoint, Vector3 midPoint, Vector3 endPoint) {
+        return (1-t)*((1-t)*startPoint + t*midPoint) + t*((1-t)*midPoint + t*endPoint);
+    }
+}
---
+++ Assets/Resources/Scripts/MouseFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.SetPositionAndRotation(Input.mousePosition/150f, transform.rotation);
+	}
+}
