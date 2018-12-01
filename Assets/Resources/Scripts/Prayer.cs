b38fbe63e37c1a30c9069514518a86f909c8200d
DevJarmo <jeremyleebuck@gmail.com>
2018-12-01 06:26:24 -0700

    Prayer Meter Work

---
+++ Assets/Resources/Scripts/Prayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Prayer : MonoBehaviour {
+
+    //Serialized Prayer Variables
+    [SerializeField] private float SpeedModifer = 1f;
+    [SerializeField] private float AngularModifer = .2f;
+
+    //Private Prayer Variables
+    private Vector3 _vehicleLocation = Vector3.zero;
+    private Vector3 _prayerMeterLocation;
+    private Rigidbody2D _prayerRigidBody;
+
+    public void Initialize(Vector3 vehicleLocation)
+    {
+        //Set the prayer's destination.
+        _prayerMeterLocation = GameObject.Find("PrayerMeterProgress").transform.position;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        //Initialize the rigid body.
+        _prayerRigidBody = gameObject.transform.Find("PrayerSprite").GetComponent<Rigidbody2D>();
+
+	    //Animate toward the progress bar.
+	    _prayerRigidBody.velocity = (_prayerMeterLocation * Time.deltaTime) * SpeedModifer;
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        //Animate toward the progress bar.
+        _prayerRigidBody.velocity = new Vector2(_prayerRigidBody.velocity.x - AngularModifer, _prayerRigidBody.velocity.y);
+    }
+}
---
+++ Assets/Resources/Scripts/PrayerMeter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class PrayerMeter : MonoBehaviour {
+
+    //Prayer Meter Serialized Fields
+    [SerializeField] private long DecayTimer = 500;
+    [SerializeField] private int DecayValue = 10;
+    [SerializeField] private int MaximumPrayers = 1000;
+    [SerializeField] private float PrayerLifeTime = .75F;
+    [SerializeField] private GameObject ProgressBar;
+    [SerializeField] private GameObject PrayerPrefab;
+
+    //Prayer Meter Private Variables
+    private Stopwatch _prayerMeterDecayTimer;
+    private float _maxPrayerMeterProgressSize = 0;
+    private RectTransform _prayerMeterProgressSize;
+    private float _prayerCount = 0;
+    private Vector2 _prayerMeterLocation = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+
+        //Initialize Prayer Meter Count
+	    _prayerCount = MaximumPrayers;
+
+	    //Initialize Prayer Meter Progress Size Based on Ui
+	    _prayerMeterProgressSize = ProgressBar.GetComponent<RectTransform>();
+        _maxPrayerMeterProgressSize = _prayerMeterProgressSize.sizeDelta.x;
+
+        //Initialize Prayer Meter Location
+	    _prayerMeterLocation = new Vector2(ProgressBar.transform.position.x, ProgressBar.transform.position.y);
+
+        //Initialize and start the Prayer Meter Decay Timer
+        _prayerMeterDecayTimer = new Stopwatch();
+        _prayerMeterDecayTimer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //Decrement the prayer meter based on decay timer.
+	    if (_prayerMeterDecayTimer.ElapsedMilliseconds >= DecayTimer)
+	    {
+            //Decrement the prayer meter.
+	        _prayerCount -= DecayValue;
+
+            //Set a bottom threshold for prayer meter count.
+	        if (_prayerCount <= 0) _prayerCount = 0;
+
+	        //Update the Ui.
+	        UpdateUi();
+
+            //Reset the decay timer.
+            _prayerMeterDecayTimer.Reset();
+	        _prayerMeterDecayTimer.Start();
+        }
+	}
+
+    public void AddPrayer(float prayerValue, Vector3 vehicleLocation)
+    {
+        //Instantiate this prayer.
+        var prayer = Instantiate(PrayerPrefab, vehicleLocation, new Quaternion(), GameObject.Find("PrayerSpace").transform);
+        prayer.GetComponent<Prayer>().Initialize(vehicleLocation);
+        Destroy(prayer, PrayerLifeTime);
+
+        //Increment the prayer count.
+        _prayerCount += prayerValue;
+
+        //Update the Ui.
+        UpdateUi();
+    }
+
+    private void UpdateUi()
+    {
+        //Update the Ui.
+        var progressUpdate = (_prayerCount / MaximumPrayers) * _maxPrayerMeterProgressSize;
+        _prayerMeterProgressSize.sizeDelta = new Vector2(progressUpdate, _prayerMeterProgressSize.sizeDelta.y);
+    }
+}
