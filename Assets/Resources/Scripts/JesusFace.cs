4094168cf6ed99c979df7188a3642d5ba9c902d5
PeachTreeOath <peachtreeoath@gmail.com>
2018-12-01 16:43:20 -0700

    Wince face put in

---
+++ Assets/Resources/Scripts/JesusFace.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JesusFace : MonoBehaviour
+{
+
+    public Sprite neutralFace;
+    public Sprite winceFace;
+    public float winceDuration;
+    private float winceTime;
+    private bool isDoneWincing;
+    private SpriteRenderer spr;
+
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        winceTime += Time.deltaTime;
+        if (!isDoneWincing && winceTime > winceDuration)
+        {
+            GotoNeutralFace();
+        }
+    }
+
+    // Should only be called internally via timer but leaving this public anyways
+    public void GotoNeutralFace()
+    {
+        spr.sprite = neutralFace;
+        isDoneWincing = true;
+    }
+
+    public void GotoWinceFace()
+    {
+        spr.sprite = winceFace;
+        winceTime = 0;
+        isDoneWincing = false;
+    }
+}
