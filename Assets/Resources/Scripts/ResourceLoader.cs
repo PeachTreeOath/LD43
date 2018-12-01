8ac878f369020b3f88e9f36156be0524d424eaa7
PeachTreeOath <peachtreeoath@gmail.com>
2018-11-30 23:48:51 -0700

    Basic scripts and scene setup

---
+++ Assets/Resources/Scripts/ResourceLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoader : Singleton<ResourceLoader>
+{
+    //[HideInInspector]
+    //public Sprite timeIcon;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        LoadResources();
+    }
+
+    private void LoadResources()
+    {
+    //timeIcon = Resources.Load<Sprite>("Textures/icons_v2/white_time");
+    }
+}
