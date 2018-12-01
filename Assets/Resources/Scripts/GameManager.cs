3171f6bea71058e915672e2d14999d8666983f23
Dave Flanagan <dflanagan314@gmail.com>
2018-12-01 11:00:43 -0500

    Added some real shitty checkpoint support

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -8,4 +8,25 @@ public class GameManager : Singleton<GameManager>
     public Vector2 bottomRightBound;
 
     public float roadSpeed;
+
+    private CheckpointManager checkpointManager;
+    private VehiclePool vehiclePool;
+
+    void Start() {
+        vehiclePool = FindObjectOfType<VehiclePool>();
+        Debug.Log("Loaded VehiclePool: " + vehiclePool.gameObject.name);
+        checkpointManager = FindObjectOfType<CheckpointManager>();
+        Debug.Log("Loaded CheckpointManager: " + checkpointManager.gameObject.name);
+
+        //start'er up
+        hitCheckpoint();
+    }
+
+    public void hitCheckpoint() {
+        checkpointManager.hitCheckpoint();
+    }
+
+    public VehiclePool getVehiclePool() {
+        return vehiclePool;
+    }
 }
