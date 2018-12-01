d2b313a2af50ccd7e34905f5442dc6a7b36c3875
Unknown <rfonger@gmail.com>
2018-12-01 01:33:45 -0700

    First bug fix

--- Assets/Resources/Scripts/LevelManager.cs
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
-    public VehicleType[] vehicleTypes = new VehicleType[];
     public float secondsBetweenCheckpoints;
     public float spawnRateInSeconds;
     public float rateOfPrayerDecay;
