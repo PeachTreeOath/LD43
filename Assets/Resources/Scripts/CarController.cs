b041476f5c4bfbed32d788d7cc963ac0f0146fd6
PeachTreeOath <peachtreeoath@gmail.com>
2018-12-01 00:45:19 -0700

    Placeholder movement

--- Assets/Resources/Scripts/CarController.cs
+++ Assets/Resources/Scripts/CarController.cs
@@ -7,15 +7,20 @@ public class CarController : MonoBehaviour
 
     public float moveSpeed;
 
+    private Rigidbody2D rbody;
+
     // Use this for initialization
     void Start()
     {
-
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float hDelta = moveSpeed * Input.GetAxisRaw("Horizontal");
+        float vDelta = moveSpeed * Input.GetAxisRaw("Vertical");
+        Vector2 newPosition = (Vector2)rbody.transform.position + new Vector2(hDelta, vDelta);
+        rbody.MovePosition(newPosition);
     }
 }
