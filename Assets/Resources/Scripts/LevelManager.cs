b60215f99581caadd461c2607a2a65fe58133cee
Unknown <rfonger@gmail.com>
2018-12-01 01:29:37 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public VehicleType[] vehicleTypes = new VehicleType[];
+    public float secondsBetweenCheckpoints;
+    public float spawnRateInSeconds;
+    public float rateOfPrayerDecay;
+
+}
