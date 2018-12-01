c836c6ade68ab5fbcf6ba01af6d7720faa634d04
krevlee <krystalevlee@gmail.com>
2018-12-01 06:10:31 -0700

    adding obstacle files that weren't tracked

---
+++ Assets/Resources/Scripts/ObstacleStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleStats : MonoBehaviour 
+{
+	public ObstacleTypeEnum obstacleType;
+
+	[Tooltip("How soon from start up should the first obstacle spawn - 0 means right away.")]
+	public float firstSpawnTime;
+
+	[Tooltip("How frequently cones should spawn after start.")]
+	public float spawnInterval;
+
+    [HideInInspector]
+	public float spawnTimer;
+	
+    [HideInInspector]
+	public float obstacleHeight;
+}
