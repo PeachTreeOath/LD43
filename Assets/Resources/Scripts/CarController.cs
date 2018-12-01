fb4a9287d50c87eef8dbe7bdac821397f82515a1
PeachTreeOath <peachtreeoath@gmail.com>
2018-12-01 01:26:36 -0700

    Basic car movement w rotation

--- Assets/Resources/Scripts/CarController.cs
+++ Assets/Resources/Scripts/CarController.cs
@@ -6,6 +6,9 @@ public class CarController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public float rotateSpeed;
+    public float angledMovementPower;
+    public GameObject carSprite; // Contains both sprite and collider and are separate from the actual GameObject
 
     private Rigidbody2D rbody;
 
@@ -18,9 +21,38 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hDelta = moveSpeed * Input.GetAxisRaw("Horizontal");
+        // Movement from input
+        float rotateDelta = rotateSpeed * Input.GetAxisRaw("Horizontal");
+        carSprite.transform.Rotate(Vector3.back, rotateDelta);
+
+        // Drift car left/right based on how much rotation applied
+        float hDelta = GetHorizontalDeltaFromRotation(carSprite.transform.eulerAngles.z);
+
         float vDelta = moveSpeed * Input.GetAxisRaw("Vertical");
-        Vector2 newPosition = (Vector2)rbody.transform.position + new Vector2(hDelta, vDelta);
+        Vector2 newPosition = (Vector2)transform.position + new Vector2(hDelta, vDelta);
         rbody.MovePosition(newPosition);
+
+        // Apply random drift
+        // TODO
+    }
+
+    private float GetHorizontalDeltaFromRotation(float eulerAngle)
+    {
+        if (eulerAngle < 180)// && eulerAngle != 360)
+        {
+            float angle = 0 - eulerAngle;
+			Debug.Log("angle " + angle + " euler " + eulerAngle);
+            float angleSeverity = Mathf.Pow(angle, 2);
+			Debug.Log("angle sev " + angleSeverity);
+            return -angleSeverity * angledMovementPower;
+        }
+        else
+        {
+            float angle = 360-eulerAngle;
+			Debug.Log("dangle " + angle + " euler " + eulerAngle);
+            float angleSeverity = Mathf.Pow(angle, 2);
+			Debug.Log("dangle sev " + angleSeverity);
+            return angleSeverity * angledMovementPower;
+        }
     }
 }
