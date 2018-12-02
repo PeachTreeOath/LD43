905411b30f419c573e725c5e65d73b86e74c2bf9
DevJarmo <jeremyleebuck@gmail.com>
2018-12-01 20:14:00 -0700

    Resource Loader update.

--- Assets/Resources/Scripts/ResourceLoader.cs
+++ Assets/Resources/Scripts/ResourceLoader.cs
@@ -43,6 +43,10 @@ public class ResourceLoader : Singleton<ResourceLoader>
     public GameObject obituaryText;
     #endregion
 
+    #region Rendering
+    public GameObject vehicleWobble;
+    #endregion
+
     protected override void Awake()
     {
         base.Awake();
@@ -58,6 +62,9 @@ public class ResourceLoader : Singleton<ResourceLoader>
 
         // Ui Elements
         vehicleSleepCaption = Resources.Load<GameObject>("Prefabs/VehicleSleepCaption");
+
+        //Rendering
+        vehicleWobble = Resources.Load<GameObject>("Prefabs/VehicleWobble");
 
         // Vehicles
         bus = Resources.Load<GameObject>("Prefabs/Vehicles/Bus");
