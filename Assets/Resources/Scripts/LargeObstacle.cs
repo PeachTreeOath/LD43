5e4e19b063b73dd9ad2e9883327b287199edbec7
DarkFortress2\Elyk Yevrah <elykyevrah@gmail.com>
2018-12-01 23:19:37 -0600

    Started Large Obsticles.

---
+++ Assets/Resources/Scripts/LargeObstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeObstacle : MonoBehaviour {
+
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public float movespeed;
+    public float minFrequency;
+    public float maxFrequency;
+
+    private float timeSinceLastOccurance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
