54f5f5d605cf3bf741840af074917832a1c32bb1
Thundercleez <tmizas@gmail.com>
2018-12-02 12:34:15 -0500

    Script was actually used. Readded to scene.

---
+++ Assets/Resources/Scripts/RayCaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controls obstacles like cones on the road.
+public class RayCaster : MonoBehaviour
+{
+    public static RaycastHit2D hitInfo;
+
+    void FixedUpdate()
+    {
+        hitInfo = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+    }
+}
