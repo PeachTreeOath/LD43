c8e25a5b0b62d6f309e1470286f091a5d8f38819
Thundercleez <tmizas@gmail.com>
2018-12-02 12:31:16 -0500

    Added highlighting vehicles on mouse over.

---
+++ Assets/Resources/Scripts/HighlightableVehicle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controls obstacles like cones on the road.
+public class HighlightableVehicle : MonoBehaviour
+{
+    public Renderer rend;
+    public VehicleController vc;
+    bool wasSelected;
+
+    void Update()
+    {
+        if(RayCaster.hitInfo.collider != null)
+        {
+            if(RayCaster.hitInfo.collider.gameObject.GetInstanceID() == gameObject.GetInstanceID())
+            {
+                wasSelected = true;
+                rend.enabled = true;
+            }else if(wasSelected)
+            {
+                rend.enabled = false;
+            }
+        }else
+        {
+            rend.enabled = false;
+        }
+
+        if(vc.isSelected && 
+            rend.enabled)
+        {
+            rend.enabled = false;
+        }
+    }
+}
