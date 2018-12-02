5e91cb3a6556fa28b38cbad8fe2eb89e069ff40a
DevJarmo <jeremyleebuck@gmail.com>
2018-12-01 21:09:53 -0700

    Screen Shake Work.

---
+++ Assets/Resources/Scripts/ScreenShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour {
+
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] private float shakeDuration;
+    [SerializeField] private float shakeMagnitude;
+    private Vector3 initialPosition;
+    private Stopwatch shakeTimer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cameraTransform = gameObject.transform;
+        initialPosition = cameraTransform.localPosition;
+        shakeTimer = new Stopwatch();
+        shakeTimer.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (shakeTimer.ElapsedMilliseconds < shakeDuration)
+	    {
+	        transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+	    }
+	    else
+	    {
+	        shakeTimer.Stop();
+	        transform.localPosition = initialPosition;
+	    }
+    }
+
+    public void TriggerShake()
+    {
+        shakeTimer.Reset();
+        shakeTimer.Start();
+    }
+}
