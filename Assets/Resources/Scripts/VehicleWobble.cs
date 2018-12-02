4830f0cf97c78888c28ef7802cb2fefca4ae5f6a
DevJarmo <jeremyleebuck@gmail.com>
2018-12-01 20:10:23 -0700

    Working on Wobble...

--- Assets/Resources/Scripts/VehicleWobble.cs
+++ Assets/Resources/Scripts/VehicleWobble.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class VehicleWobble : MonoBehaviour {
+
+    //SAdjust wobble through Inspector Prefabs Vehicle Wobble
+    [SerializeField] bool enableWobble = true;
+    [SerializeField] float wobbleMin;
+    [SerializeField] float wobbleMax;
+    [SerializeField] float wobbleFrequency;
 
     //Starting Position
     private GameObject vehicleSpriteObject;
     private Transform vehicleSpriteTransform;
     private Vector2 startingPos;
+    private Stopwatch wobbleTimer;
+
 
 	// Use this for initialization
 	void Start ()
@@ -16,13 +25,18 @@ public class VehicleWobble : MonoBehaviour {
 	    vehicleSpriteTransform = vehicleSpriteObject.GetComponent<Transform>();
 	    startingPos.x = vehicleSpriteTransform.transform.position.x;
 	    startingPos.y = vehicleSpriteTransform.transform.position.y;
+
+        wobbleTimer = new Stopwatch();
+        wobbleTimer.Start();
     }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    var random = Random.Range(1, 1.65f);
-	    vehicleSpriteObject.transform.position = new Vector2(startingPos.x + Mathf.Sin(Time.deltaTime) * random,
+	    if (!enableWobble) return;
+	    var random = Random.Range(wobbleMin, wobbleMax);
+	    var time = Random.Range(timeMin, timeMax);
+        vehicleSpriteObject.transform.position = new Vector2(startingPos.x + Mathf.Sin(Time.deltaTime) * random,
 	        startingPos.y + Mathf.Sin(Time.deltaTime) * random);
 	}
 }
