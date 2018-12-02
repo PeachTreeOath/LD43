9efd3c825c7f0fdafd89c51952a54b40a52ed164
Thundercleez <tmizas@gmail.com>
2018-12-01 19:14:18 -0500

    Light shaft no longer rotates with car. Added clouds above light shaft.

---
+++ Assets/Resources/Scripts/ObjectFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class ObjectFollower : MonoBehaviour
+{
+    public Transform target;
+
+    Vector3 followOffset;
+
+    void Start()
+    {
+        followOffset = gameObject.transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        gameObject.transform.position = target.position + followOffset;
+    }
+}
