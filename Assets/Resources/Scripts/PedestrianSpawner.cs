7a3b683652ea0f4c6cc1207de45cbd73160f6b7d
mjeffery <michael.jeffery@gmail.com>
2018-12-03 01:41:27 -0500

    add pedestrian spawner

--- Assets/Resources/Scripts/PedestrianSpawner.cs
+++ Assets/Resources/Scripts/PedestrianSpawner.cs
@@ -1,7 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PedestrianSpawner : MonoBehaviour {
+public class PedestrianSpawner : ProgrammableSpawner {
 
+    public ObstacleStats obstacleStats;
 
+    protected override void Spawn() {
+        bool[] spawnAt = GetSpawnLocations();
+        for(var i = 0; i < spawnAt.Length; i++) {
+            if (spawnAt[i]) SpawnPedestrian(i);
+        }
+    }
+
+    private void SpawnPedestrian(int lane) {
+        float randomX = UnityEngine.Random.Range(GameManager.instance.upperLeftBound.x, GameManager.instance.bottomRightBound.x);
+        float randomY = UnityEngine.Random.Range(GameManager.instance.upperLeftBound.y, GameManager.instance.bottomRightBound.y);
+
+        Vector2 startPosition = new Vector2(lanes[lane] + Median.WIDTH_PER_LANE / 2f, 6f);
+		Vector3 endPosition = new Vector3(randomX, randomY, 0);
+
+        var obstacleGO = ResourceLoader.instance.obstaclePedestrianPrefab;
+        var obstacle = Instantiate(obstacleGO, startPosition, Quaternion.identity).GetComponent<ObstacleController>();
+        obstacle.endPosition = endPosition;
+        obstacle.obstacleStats = obstacleStats;
+    }
 }
